4404b3949593545de7e184138d6bbe52b0f8b702
HoangQuanGITHUB <mcinotgoodatgame@gmail.com>
2023-12-06 06:28:16 +0700

    Buttons added

--- App/Form1.Designer.cs
+++ App/Form1.Designer.cs
@@ -35,26 +35,65 @@ namespace App
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             flowLayoutPanel1 = new FlowLayoutPanel();
+            flowLayoutPanel3 = new FlowLayoutPanel();
+            bai1 = new Button();
             flowLayoutPanel2 = new FlowLayoutPanel();
             button1 = new Button();
             button2 = new Button();
-            button3 = new Button();
             pdfViewer1 = new Apitron.PDF.Controls.PDFViewer();
+            bai2 = new Button();
+            bai3 = new Button();
+            bai4 = new Button();
+            bai5 = new Button();
+            bai6 = new Button();
+            bai7 = new Button();
+            bai8 = new Button();
+            bai9 = new Button();
+            bai10 = new Button();
+            bai11 = new Button();
+            bai12 = new Button();
+            flowLayoutPanel1.SuspendLayout();
+            flowLayoutPanel3.SuspendLayout();
             flowLayoutPanel2.SuspendLayout();
             SuspendLayout();
             // 
             // flowLayoutPanel1
             // 
             flowLayoutPanel1.BackColor = Color.FromArgb(21, 23, 31);
+            flowLayoutPanel1.Controls.Add(flowLayoutPanel3);
             resources.ApplyResources(flowLayoutPanel1, "flowLayoutPanel1");
             flowLayoutPanel1.Name = "flowLayoutPanel1";
+            // 
+            // flowLayoutPanel3
+            // 
+            flowLayoutPanel3.Controls.Add(bai1);
+            flowLayoutPanel3.Controls.Add(bai2);
+            flowLayoutPanel3.Controls.Add(bai3);
+            flowLayoutPanel3.Controls.Add(bai4);
+            flowLayoutPanel3.Controls.Add(bai5);
+            flowLayoutPanel3.Controls.Add(bai6);
+            flowLayoutPanel3.Controls.Add(bai7);
+            flowLayoutPanel3.Controls.Add(bai8);
+            flowLayoutPanel3.Controls.Add(bai9);
+            flowLayoutPanel3.Controls.Add(bai10);
+            flowLayoutPanel3.Controls.Add(bai11);
+            flowLayoutPanel3.Controls.Add(bai12);
+            resources.ApplyResources(flowLayoutPanel3, "flowLayoutPanel3");
+            flowLayoutPanel3.Name = "flowLayoutPanel3";
+            // 
+            // button3
+            // 
+            resources.ApplyResources(bai1, "button3");
+            bai1.ForeColor = SystemColors.ButtonFace;
+            bai1.Name = "button3";
+            bai1.UseVisualStyleBackColor = false;
+            bai1.Click += bai1f;
             // 
             // flowLayoutPanel2
             // 
             flowLayoutPanel2.BackColor = Color.FromArgb(21, 23, 31);
             flowLayoutPanel2.Controls.Add(button1);
             flowLayoutPanel2.Controls.Add(button2);
-            flowLayoutPanel2.Controls.Add(button3);
             resources.ApplyResources(flowLayoutPanel2, "flowLayoutPanel2");
             flowLayoutPanel2.Name = "flowLayoutPanel2";
             flowLayoutPanel2.Paint += flowLayoutPanel2_Paint;
@@ -65,36 +104,126 @@ namespace App
             button1.ForeColor = SystemColors.Control;
             button1.Name = "button1";
             button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
             // 
             // button2
             // 
             resources.ApplyResources(button2, "button2");
             button2.ForeColor = SystemColors.ButtonFace;
             button2.Name = "button2";
-            button1.Click += button2_Click;
             button2.UseVisualStyleBackColor = false;
-            // 
-            // button3
-            // 
-            resources.ApplyResources(button3, "button3");
-            button3.ForeColor = SystemColors.ButtonFace;
-            button3.Name = "button3";
-            button3.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
             // 
             // pdfViewer1
             // 
-             resources.ApplyResources(pdfViewer1, "pdfViewer1");
-            FileStream fs = new FileStream("Documents/Python.pdf", FileMode.Open);
-            Document document = new Document(fs);
-            pdfViewer1.Dock = DockStyle.Bottom;
-            pdfViewer1.Anchor = AnchorStyles.Right|AnchorStyles.Left|AnchorStyles.Top|AnchorStyles.Bottom;
-            pdfViewer1.Document = document;
+            resources.ApplyResources(pdfViewer1, "pdfViewer1");
+            pdfViewer1.Document = null;
             pdfViewer1.EnableSearch = true;
             pdfViewer1.Name = "pdfViewer1";
             pdfViewer1.RenderingQuality = Apitron.PDF.Controls.RenderingQuality.High;
             pdfViewer1.SearchHighlightColor = Color.FromArgb(128, 255, 255, 0);
-            
-           
+            // 
+            // button4
+            // 
+            resources.ApplyResources(bai2, "button4");
+            bai2.ForeColor = SystemColors.ButtonFace;
+            bai2.Name = "button4";
+            bai2.UseVisualStyleBackColor = false;
+            bai2.Click += bai2f;
+            // 
+            // button5
+            // 
+            resources.ApplyResources(bai3, "button5");
+            bai3.ForeColor = SystemColors.ButtonFace;
+            bai3.Name = "button5";
+            bai3.UseVisualStyleBackColor = false;
+            bai3.Click += bai3f;
+            // 
+            // button6
+            // 
+            resources.ApplyResources(bai4, "button6");
+            bai4.ForeColor = SystemColors.ButtonFace;
+            bai4.Name = "button6";
+            bai4.UseVisualStyleBackColor = false;
+            bai4.Click += bai4f;
+            // 
+            // button7
+            // 
+            resources.ApplyResources(bai5, "button7");
+            bai5.ForeColor = SystemColors.ButtonFace;
+            bai5.Name = "button7";
+            bai5.UseVisualStyleBackColor = false;
+            bai5.Click += bai5f;
+            // 
+            // button8
+            // 
+            resources.ApplyResources(bai6, "button8");
+            bai6.ForeColor = SystemColors.ButtonFace;
+            bai6.Name = "button8";
+            bai6.UseVisualStyleBackColor = false;
+            bai6.Click += bai6f;
+            // 
+            // button9
+            // 
+            resources.ApplyResources(bai7, "button9");
+            bai7.ForeColor = SystemColors.ButtonFace;
+            bai7.Name = "button9";
+            bai7.UseVisualStyleBackColor = false;
+            bai7.Click += bai7f;
+            // 
+            // button10
+            // 
+            resources.ApplyResources(bai8, "button10");
+            bai8.ForeColor = SystemColors.ButtonFace;
+            bai8.Name = "button10";
+            bai8.UseVisualStyleBackColor = false;
+            bai8.Click += bai8f;
+            // 
+            // button11
+            // 
+            resources.ApplyResources(bai9, "button11");
+            bai9.ForeColor = SystemColors.ButtonFace;
+            bai9.Name = "button11";
+            bai9.UseVisualStyleBackColor = false;
+            bai9.Click += bai9f;
+            // 
+            // button12
+            // 
+            resources.ApplyResources(bai10, "button12");
+            bai10.ForeColor = SystemColors.ButtonFace;
+            bai10.Name = "button12";
+            bai10.UseVisualStyleBackColor = false;
+            bai10.Click += bai10f;
+            // 
+            // button13
+            // 
+            resources.ApplyResources(bai11, "button13");
+            bai11.ForeColor = SystemColors.ButtonFace;
+            bai11.Name = "button13";
+            bai11.UseVisualStyleBackColor = false;
+            bai11.Click += bai11f;
+            // 
+            // button14
+            // 
+            resources.ApplyResources(bai12, "button14");
+            bai12.ForeColor = SystemColors.ButtonFace;
+            bai12.Name = "button14";
+            bai12.UseVisualStyleBackColor = false;
+            bai12.Click += bai12f;
+
+            bai1.Visible=false;
+            bai2.Visible=false;
+            bai3.Visible=false;
+            bai4.Visible=false;
+            bai5.Visible=false;
+            bai6.Visible=false;
+            bai7.Visible=false;
+            bai8.Visible=false;
+            bai9.Visible=false;
+            bai10.Visible=false;
+            bai11.Visible=false;
+            bai12.Visible=false;
+
             // 
             // Form1
             // 
@@ -107,8 +236,15 @@ namespace App
             Name = "Form1";
             WindowState = FormWindowState.Maximized;
             Load += Form1_Load;
+            flowLayoutPanel1.ResumeLayout(false);
+            flowLayoutPanel3.ResumeLayout(false);
             flowLayoutPanel2.ResumeLayout(false);
             ResumeLayout(false);
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         #endregion
@@ -116,7 +252,19 @@ namespace App
         private FlowLayoutPanel flowLayoutPanel2;
         private Button button1;
         private Button button2;
-        private Button button3;
         private Apitron.PDF.Controls.PDFViewer pdfViewer1;
+        private FlowLayoutPanel flowLayoutPanel3;
+        private Button bai1;
+        private Button bai2;
+        private Button bai3;
+        private Button bai4;
+        private Button bai5;
+        private Button bai6;
+        private Button bai7;
+        private Button bai8;
+        private Button bai9;
+        private Button bai10;
+        private Button bai11;
+        private Button bai12;
     }
 }
--- App/Form1.cs
+++ App/Form1.cs
@@ -1,8 +1,11 @@
 using Apitron.PDF.Rasterizer;
+using App.Properties;
+using System.Resources;
 namespace App
 {
     public partial class Form1 : Form
     {
+        public string current_doc = "";
         public Form1()
         {
             InitializeComponent();
@@ -27,13 +30,116 @@ namespace App
         {
 
         }
-         private void button2_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
-            FileStream fs = new FileStream("Documents/Python.pdf", FileMode.Open);
- Document document = new Document(fs);
-pdfViewer1.Dock = DockStyle.Bottom;
-pdfViewer1.Anchor = AnchorStyles.Right;
-pdfViewer1.Document = document;
+            FileStream fs = new FileStream("Documents/C++/bai1.pdf", FileMode.Open);
+            Document document = new Document(fs);
+            pdfViewer1.Document = document;
+            current_doc="C++";
+            bai1.Visible=true;
+            bai2.Visible=true;
+            bai3.Visible=true;
+            bai4.Visible=true;
+            bai5.Visible=true;
+            bai6.Visible=true;
+            bai7.Visible=true;
+            bai8.Visible=true;
+            bai9.Visible=true;
+            bai10.Visible=false;
+            bai11.Visible=false;
+            bai12.Visible=false;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream("Documents/Python/bai1.pdf", FileMode.Open);
+            Document document = new Document(fs);
+            pdfViewer1.Document = document;
+            current_doc="Python";
+            bai1.Visible=true;
+            bai2.Visible=true;
+            bai3.Visible=true;
+            bai4.Visible=true;
+            bai5.Visible=true;
+            bai6.Visible=true;
+            bai7.Visible=true;
+            bai8.Visible=true;
+            bai9.Visible=true;
+            bai10.Visible=true;
+            bai11.Visible=true;
+            bai12.Visible=true;
+        }
+
+        private void bai1f(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream($"Documents/{current_doc}/bai1.pdf", FileMode.Open);
+            Document document = new Document(fs);
+            pdfViewer1.Document = document;
+        }
+        private void bai2f(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream($"Documents/{current_doc}/bai2.pdf", FileMode.Open);
+            Document document = new Document(fs);
+            pdfViewer1.Document = document;
+        }
+        private void bai3f(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream($"Documents/{current_doc}/bai3.pdf", FileMode.Open);
+            Document document = new Document(fs);
+            pdfViewer1.Document = document;
+        }
+        private void bai4f(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream($"Documents/{current_doc}/bai4.pdf", FileMode.Open);
+            Document document = new Document(fs);
+            pdfViewer1.Document = document;
+        }
+        private void bai5f(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream($"Documents/{current_doc}/bai5.pdf", FileMode.Open);
+            Document document = new Document(fs);
+            pdfViewer1.Document = document;
+        }
+        private void bai6f(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream($"Documents/{current_doc}/bai6.pdf", FileMode.Open);
+            Document document = new Document(fs);
+            pdfViewer1.Document = document;
+        }
+        private void bai7f(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream($"Documents/{current_doc}/bai7.pdf", FileMode.Open);
+            Document document = new Document(fs);
+            pdfViewer1.Document = document;
+        }
+        private void bai8f(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream($"Documents/{current_doc}/bai8.pdf", FileMode.Open);
+            Document document = new Document(fs);
+            pdfViewer1.Document = document;
+        }
+        private void bai9f(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream($"Documents/{current_doc}/bai9.pdf", FileMode.Open);
+            Document document = new Document(fs);
+            pdfViewer1.Document = document;
+        }
+        private void bai10f(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream($"Documents/{current_doc}/bai10.pdf", FileMode.Open);
+            Document document = new Document(fs);
+            pdfViewer1.Document = document;
+        }
+        private void bai11f(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream($"Documents/{current_doc}/bai11.pdf", FileMode.Open);
+            Document document = new Document(fs);
+            pdfViewer1.Document = document;
+        }
+        private void bai12f(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream($"Documents/{current_doc}/bai12.pdf", FileMode.Open);
+            Document document = new Document(fs);
+            pdfViewer1.Document = document;
         }
     }
 }
